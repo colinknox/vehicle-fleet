263597ebce6d76ea24a0c1cb0e84a6a69b86fe98
colinknox <colinwknox@gmail.com>
2026-02-01 20:42:53 -0500

    added starter code. made 3 classes with 2 of them being child classes of the third class.

---
+++ main.py
@@ -0,0 +1,33 @@
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_description(self):
+        return f"{self.year} {self.make} {self.model}"
+    
+    def age(self):
+        return 2026 - self.year
+
+class Car(Vehicle):
+    def __init__(self, make, model, year, num_doors):
+        super().__init__(make, model, year)
+        self.num_doors = num_doors
+
+    def get_door_info(self):
+        return f"This car has {self.num_doors} doors"
+
+class Motorcycle(Vehicle):
+    def __init__(self, make, model, year, has_sidecar):
+        super().__init__(make, model, year)
+        self.has_sidecar = has_sidecar
+
+
+
+brough = Motorcycle("Brough", "Superior", 1932, False)
+
+print(brough.make)
+print(brough.model)
+print(brough.year)
+print(brough.has_sidecar)
